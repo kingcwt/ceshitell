3925c23172f334d7af8d2b221c80b150e46d4513
kingcwt <cuihongran@goyoo.com>
2019-12-02 21:50:15 +0800

    u

--- blur.js
+++ blur.js
@@ -3,6 +3,7 @@ var blufn=function () {
     // $(document).height() ==>document.documentElement.offsetHeight  =>整个文档的高度
     // $(window).height() ==>document.documentElement.clientHeight  => 可见区域的高度
     var h = $(document).height()-$(window).height();
+    console.log(h);
     $(document).scrollTop(h);
 };
 $(":input").on('blur', blufn);
